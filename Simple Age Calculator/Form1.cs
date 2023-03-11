f02ed4b2fb6657533c0f077f6cca0b3f99742d21
karimAkram <karim.akram@cashcall.com.eg>
2023-03-11 15:29:46 +0200

    Simple Age Calculator App

--- Simple Age Calculator/Form1.cs
+++ Simple Age Calculator/Form1.cs
@@ -6,5 +6,30 @@ namespace Simple_Age_Calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int age;
+                age = Convert.ToInt32(birthYear.Text);
+                Result.Text = Convert.ToString(DateTime.Now.Year-age);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Attention Please Enter the correct year of birth in numbers!!");
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
